f77689c0d60c5f6a5e7ff0f36fd3e46c36bd5045
Qianqi Kay Shen <qianqi.shen@gmail.com>
2017-04-05 20:30:17 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 # the contents of this script could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
-python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
+python ./src/process_log.py  ./src/feature1.py ./src/feature2.py ./src/feature3.py ./src/feature4.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
 
